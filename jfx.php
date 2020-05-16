4b63a439da8aac763b7b338a0d753481e1fe13e0
abuusamah <49653679+abuusamah@users.noreply.github.com>
2020-05-16 12:28:32 +0700

    Update jfx.php

--- jfx.php
+++ jfx.php
@@ -67,7 +67,7 @@ echo color("white","                  Format Kode 62*** \n");
         echo "\n".color("white"," Please wait");
         for($a=1;$a<=3;$a++){
         echo color("white",".");
-        sleep(20);
+        sleep(30);
         }
         $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EBADAHMAKANA"}');
         $message = fetch_value($code1,'"message":"','"');
